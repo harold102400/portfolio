add2acbc0ce353e0d8102e320eb944ae9136d0dd
Harold Montes <haroldjosehd24@gmail.com>
2022-11-22 15:05:39 -0500

    first commit

---
+++ components/app.js
@@ -0,0 +1,7 @@
+const submitButton = document.querySelector(".submit-button");
+
+submitButton.addEventListener("click", e =>{
+    //alert("Hola");
+    e.preventDefault();
+    document.getElementById("submit-button")
+})
