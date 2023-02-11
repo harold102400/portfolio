7c4c7722ca4de600600543956a683e4a5c822177
Harold Montes <haroldjosehd24@gmail.com>
2023-02-11 07:39:57 -0500

    rename path of css and imgs

--- components/app.js
+++ components/app.js
@@ -3,5 +3,5 @@ const submitButton = document.querySelector(".submit-button");
 submitButton.addEventListener("click", e =>{
     //alert("Hola");
     e.preventDefault();
-    document.getElementById("submit-button")
 })
+
